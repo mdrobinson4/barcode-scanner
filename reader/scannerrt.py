a2c902e66ef5f3c01a4b2a324267ae80205901e7
Mark Robinson <mdrobinson4@crimson.ua.edu>
2019-07-15 08:59:57 -0500

    Update scannerrt.py

--- reader/scannerrt.py
+++ reader/scannerrt.py
@@ -6,57 +6,61 @@ import imutils
 import time
 import cv2
 import info
+import threading
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-o", "--output", type=str, default="barcodes.csv",
-        help="path to output CSV file containing barcodes")
-args = vars(ap.parse_args())
+def getInfo(barcodeData, email, password):
+        # get the dell device's name
+        name = info.name(barcodeData)
+        # get the dell device's ship date
+        shipDate = info.shipDate(barcodeData)
+        if name == True and shipDate == True:
+                print("got {} -- {} -- {}".format(barcodeData, name, shipDate))
+                # uploadDate
+        return
+        
 
-# initialize video stream
-print("[INFO] starting video stream...")
-vs = VideoStream(usePiCamera=True).start()
-time.sleep(2.0)
+def scan(vs):
+        found = set()
+        while True:
+            frame = vs.read()
+            frame = imutils.resize(frame, width=400)
 
-# open the output csv file for writing and initializing the set of barcodes
-csv = open(args["output"], "w")
-found = set()
+            # find the barcodes and decode
+            barcodes = pyzbar.decode(frame)
 
-while True:
-    frame = vs.read()
-    frame = imutils.resize(frame, width=400)
+            for barcode in barcodes:
+                (x, y, w, h) = barcode.rect
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                barcodeData = barcode.data.decode("utf-8")
+                barcodeType = barcode.type
+                # if barcode is unique add it to the found set
+                # and get warrenty info / upload it to the server in a thread
+                if barcodeData not in found:
+                        found.add(barcodeData)
+                        # get warrenty info / upload to server
+                        deviceInfoThread = threading.Thread(target=getInfo, args=(barcodeData, email, password))
+                # write barcode data and type to image
+                text = "{} ({})".format(barcodeType, barcodeData)
+                cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-    # find the barcodes and decode
-    barcodes = pyzbar.decode(frame)
+            cv2.imshow("Barcode Scanner", frame)
 
-    for barcode in barcodes:
-        (x, y, w, h) = barcode.rect
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
+                break
 
-        barcodeData = barcode.data.decode("utf-8")
-        barcodeType = barcode.type
-        # print info derived from barcode
-        print("Data: {}".format(barcodeData))
-        print("Name: {}".format(info.name(barcodeData)))
-        print("Ship Date: {}".format(info.shipDate(barcodeData)))
-        print()
 
-        text = "{} ({})".format(barcodeData, barcodeType)
-        cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-        # if barcode text not in csv, write to csv
-        if barcodeData not in found:
-            csv.write("{},{}\n".format(datetime.datetime.now(), barcodeData))
-            csv.flush()
-            found.add(barcodeData)
-            
-    cv2.imshow("Barcode Scanner", frame)
-    
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
-        break
+if __name__ == '__main__':
+        # initialize video stream
+        print("[INFO] starting video stream...")
+        vs = VideoStream(usePiCamera=True).start()
+        time.sleep(2.0)
+        scan(vs)
+        print("[INFO] cleaning up...")
+        csv.close()
+        cv2.destroyAllWindows()
+        vs.stop()
 
-print("[INFO] cleaning up...")
-csv.close()
-cv2.destroyAllWindows()
-vs.stop()
-
+        
+        
