ef830c3588850c97b72ea8ebd7a00b3fa02046a8
Mark Robinson <mdrobinson4@crimson.ua.edu>
2019-07-15 21:19:57 -0500

    Update routes.js

--- routes.js
+++ routes.js
@@ -10,7 +10,7 @@ const passport = require("passport");
 
 const User = require("./database/user");
 const Group = require("./database/group");
-const Barcode = require("./database/barcode")
+const Barcode = require("./database/barcode");
 
 /*  Create Account */
 app.post("/create-account", (req, res) => {
@@ -50,24 +50,25 @@ app.post("/login", passport.authenticate("local"), (req, res) => {
 
 // upload a barcode
 app.post("/upload-barcode", (req, res) => {
-    let user = User.findOne(
-    {'email': req.body.email, 'password': req.body.password},
+  let user = User.findOne(
+    { email: req.body.email, password: req.body.password },
     (err, user) => {
       if (!err && user) {
         const barcode = new Barcode({
-            data: req.body
+          data: req.body
         });
         barcode.save((err, barcode) => {
-        if (!err && barcode) {
-            user.barcodes.push(barcode._id)
-        }
-        res.status(200).send({barcode, user})
-        }
-      )
+          if (!err && barcode) {
+            user.barcodes.push(barcode._id);
+          }
+          res.status(200).send({ barcode, user });
+        });
+      }
     }
-    res.status(404).send({err})
-})
-    
+  );
+  res.status(404).send({ err });
+});
+
 // gets all of the user's barcodes
 app.get("/barcodes", (req, res) => {
   // get all of their barcodes
@@ -77,113 +78,20 @@ app.get("/barcodes", (req, res) => {
   res.send(404).send({ error });
 });
 
-
 // gets a specific barcode
 // sent barcode id
 app.get("/barcode", (req, res) => {
   // find the barcode
-  Barcode.find(
-    { _id: req.body.id },
-    (err, barcode) => {
-      if (!err && barcode.length) {
-        res.status(200).send({success: true, barcode});
-      }
+  Barcode.find({ _id: req.body.id }, (err, barcode) => {
+    if (!err && barcode.length) {
+      res.status(200).send({ success: true, barcode });
     }
-  )
-  res.status(404).send({success: false})
-})
-  
+  });
+  res.status(404).send({ success: false });
+});
+
 module.exports = app;
 
 /////////////////////////////////////////////////
-  
 
 // const upload = require("./database/aws ")();
-
-// create a new group
-  /*
-app.post("/create-group", (req, res) => {
-  let user = User.findOne(
-    {'email': req.body.email, 'password': req.body.password},
-    (err, user) => {
-      if (!err && user) {
-        // create group
-        const group = new Group({
-          owner: user._id,
-          name: req.body.name,
-          barcodes: [],
-          description: ""
-        });
-        // save the group
-        group.save((err, group) => {
-          if (!err && group) {
-            // add group to user
-            user.groups.push(group._id);
-            user.save();
-            res.status(200).send({success: true, group});
-          }
-        })
-      }
-    }
-  );
-  // error 
-  res.status(400).send({success: false, err});
-});
-
-// upload a new barcode
-app.post("/upload-barcode", (req, res) => {
-  // get the user
-  let user = User.findOne(
-    {'email': req.body.email, 'password': req.body.password},
-    (err, user) => {
-      if (!err && user) {
-        // create new barcode
-        const barcode = new Barcode({
-          group: req.body.group,
-          name: req.body.name,
-          data: req.body
-        });
-        // save barcode
-        barcode.save((err, barcode) => {
-          if (!err && barcode) {
-            user.groups.push(barcode._id);
-            user.save();
-            res.status(200).send({success: true, barcode});
-          }
-        });
-      }
-    }
-  );
-  res.status(404).send({success: false})
-});
-
-// get all of the group names
-// sent user
-app.get("/groups", (req, res) => {
-  const groups = req.user.groups;
-  // find all of the groups
-  Group.find( 
-    { $elemMatch: { $in: groups } },
-    (err, groups) => {
-      if (!err && groups.length) {
-        console.log(groups);
-        res.status(200).send({success: true, groups});
-      }
-  });
-  res.status(404).send({success: false});
-});
-
-// get a specific group
-// sent group id
-app.get("/group", (req, res) => {
-  Group.findOne(
-    { _id: req.body.id },
-    (err, group) => {
-      if (!err && group) {
-        res.status(200).send({success: true, group});
-      }
-    }
-  )
-  res.status(404).send({success: false})
-});
-*/
